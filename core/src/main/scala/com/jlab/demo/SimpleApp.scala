be776eae327cca891cdc202171e494def56e482b
scorpiovn <nvtie@yahoo.com>
2014-11-09 01:47:05 +0700

    sample

--- core/src/main/scala/com/jlab/demo/SimpleApp.scala
+++ core/src/main/scala/com/jlab/demo/SimpleApp.scala
@@ -6,10 +6,10 @@ object SimpleApp {
   def main(args: Array[String]) {
     val logFile = "/home/scorpiovn/apps/README.md" // Should be some file on your system
     val conf = new SparkConf()
-    				.setMaster("local[2]")
-    				.setAppName("Simple Application")
-    				.setSparkHome("/home/scorpiovn/apps/spark-1.1.0-bin-hadoop2.4")
-    				.setJars(Array("target/core-1.0.0.jar"))
+                .setMaster("local[2]")
+                .setAppName("Simple Application")
+                .setSparkHome("/home/scorpiovn/apps/spark-1.1.0-bin-hadoop2.4")
+                .setJars(Array("target/core-1.0.0.jar"))
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
