d478add59d7c344ac7ab80a6aabef1fb60fbd116
njosephef <njosephef@gmail.com>
2014-11-20 01:48:28 -0500

    HOME

--- core/src/main/scala/com/jlab/demo/ScalaJob.scala
+++ core/src/main/scala/com/jlab/demo/ScalaJob.scala
@@ -7,11 +7,11 @@ import org.apache.spark.SparkContext
  */
 object ScalaJob {
   def main (args: Array[String]) {
-    val logFile = System.getenv ("SPARK_HOME") + "/prisonbreakfirst/Prison.Break.S01E01.720p.BluRay.x264-HALCYON.srt"
+    val logFile = System.getenv ("HOME") + "/prisonbreakfirst/Prison.Break.S01E01.720p.BluRay.x264-HALCYON.srt"
     val sc = new SparkContext ("spark://localhost.localdomain:7077"
 				, "Simple Job"
 				, System.getenv ("SPARK_HOME")
-				, Seq (System.getenv ("SPARK_HOME") + "/git/maven-example/core/target/core-1.0.0.jar"))
+				, Seq (System.getenv ("HOME") + "/git/maven-example/core/target/core-1.0.0.jar"))
     val logData = sc.textFile (logFile)
     val numsa = logData.filter (line => line.contains ("a")).count
     val numsb = logData.filter (line => line.contains ("b")).count
