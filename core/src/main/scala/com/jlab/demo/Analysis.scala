d2e58a675d9d92cde8b9a21e805b1c4cdd6e7576
scorpiovn <nvtie@yahoo.com>
2014-11-30 21:53:29 +0700

    analysis data

--- core/src/main/scala/com/jlab/demo/Analysis.scala
+++ core/src/main/scala/com/jlab/demo/Analysis.scala
@@ -1,14 +1,31 @@
 package com.jlab.demo
 
 import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._ // import the implicit conversions from SparkContext
 
 /**
  * Created by scorpiovn on 11/25/14.
  */
-class Analysis(context: SparkContext, file: String) {
+class Analysis(context: SparkContext, file: String) extends Serializable {
+
+  def this(context: SparkContext) {
+    this(context, "");
+  }
+
   def process(out: String): Unit = {
-    val data = context.textFile (file);
-    data.saveAsTextFile(out);
+    val regex = """[0-9,.:;',.\/<>?\-\"]""".r
+
+    val data = context.textFile(file)
+
+    data.flatMap(line => line.split("[\\s]"))
+      .map(w => regex.replaceAllIn(w.trim.toLowerCase, ""))
+      .filter(w => !w.isEmpty)
+      .map(w => (w,1))
+      .reduceByKey(_+_)
+      .map(item => item.swap)
+      .groupBy(_._1)
+      .sortBy(_._1, false)
+      .saveAsTextFile(out);
   }
 
   def process(): Unit = {
