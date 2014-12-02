de162e55c7c647d234517ccf0b2897c18b9ad6e4
scorpiovn <nvtie@yahoo.com>
2014-12-02 07:20:17 +0700

    serializer

--- core/src/main/scala/com/jlab/demo/ScalaJob.scala
+++ core/src/main/scala/com/jlab/demo/ScalaJob.scala
@@ -1,11 +1,11 @@
 package com.jlab.demo
 
+
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+import com.twitter.chill.ScalaKryoInstantiator
 
-import scala.reflect.io.Path
-import scala.util.Try
-
+import scala.util.matching.Regex
 
 /**
  * Created by ngvtien on 11/18/2014.
@@ -13,11 +13,17 @@ import scala.util.Try
 object ScalaJob {
   def main (args: Array[String]) {
 
+    val instantiator = (new ScalaKryoInstantiator).setRegistrationRequired(true)
+    val kryo = instantiator.newKryo
+    kryo.register(classOf[Regex])
+
     val logFile = System.getenv ("HOME") + "/data/prisonbreakfirst/*.srt"
     val conf = new SparkConf()
       .setAppName("SimpleJob")
       .setSparkHome(System.getenv("SPARK_HOME"))
-      //.setJars(Array(System.getenv ("HOME") + "/git/maven-example/core/target/core-1.0.0.jar"))
+//      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+//      .set("spark.kryo.registrator", "com.jlab.demo.MyRegistrator")
+//      .setJars(Array(System.getenv ("HOME") + "/git/maven-example/core/target/core-1.0.0.jar"))
 
     val sc = new SparkContext(conf)
 
