29e850e93536f3a78d8a2d60ae40daea9ecd30e1
scorpiovn <nvtie@yahoo.com>
2014-12-08 23:05:50 +0700

    suffix

--- core/src/main/scala/com/jlab/demo/IOManager.scala
+++ core/src/main/scala/com/jlab/demo/IOManager.scala
@@ -1,14 +1,23 @@
 package com.jlab.demo
 
-import scala.reflect.io.Path
-import scala.util.Try
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs._
 
 /**
  * Created by scorpiovn on 11/30/14.
  */
 object IOManager {
-  def delete(dir: String) {
-    val path: Path = Path(dir)
-    Try(path.deleteRecursively())
+
+  val hadoopConfig = new Configuration()
+  val hdfs = FileSystem.get(hadoopConfig)
+
+  def delete(path: String): Unit = {
+    if(hdfs.exists(new Path(path))) {
+      hdfs.delete(new Path(path), true) // delete file, true for recursive
+    }
+  }
+
+  def merge(srcDir: String, dstFile: String): Unit = {
+    FileUtil.copyMerge(hdfs, new Path(srcDir), hdfs, new Path(dstFile), false, hadoopConfig, null)
   }
 }
